f2ba3a901b51211aca88a2806f305a62bb8de07b
duydep24022006 <memay26032006@gmail.com>
2026-01-26 23:22:50 +0700

    SS2

--- it203/src/SS2/src/bai6.java
+++ it203/src/SS2/src/bai6.java
@@ -7,15 +7,21 @@ public class bai6 {
         int min = Integer.MAX_VALUE;
 
         int n,sum = 0;
+        double hotel=0;
         for (int i = 2; i < 9; i++) {
             String day = (i == 8) ? "Chủ nhật" : "ngày thứ " + i;
             System.out.print("Nhập lượt mượn " + day + ": ");
             n= sc.nextInt();
+            if(n==0){
+               continue;
+            }
             if(max<n) max=n;
             if(min>n)min=n;
             sum+=n;
+            hotel+=1;
+
         }
-        double avg = sum / 7.0;
+        double avg = sum / hotel;
         System.out.println("--- KẾT QUẢ THỐNG KÊ ---");
         System.out.println("Lượt mượn cao nhất: "+max);
         System.out.println("Lượt mượn ít nhất: "+min);
